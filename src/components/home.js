12b849e6efd0fc6ccf2ef13ae82dd3e8d1414e31
Kylie Lin <kylieclin@gmail.com>
2019-04-16 13:33:23 -0700

    home.js added to component folder function component to start bootstrap has not apply yet

---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Router, Switch} from 'react-router-dom';
+
+export default props =>{
+    return(
+        <div className="home-page">
+            <h1>Home page is here</h1>
+            <div className="row">
+                <button className="start-btn">Start Btn here</button>
+            </div>
+        </div>
+    )
+}
