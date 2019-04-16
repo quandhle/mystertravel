9d2507f129ebed2e4c938cd8fc2d6cfe0a223809
Quan Le <quanle@Quans-MacBook-Pro-2.local>
2019-04-16 13:25:30 -0700

    set up initial files for php

---
+++ public/api/config.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once('functions.php');
+set_exception_handler('handleError');
+
+session_start();
+handleCors();
+
+require_once('mysqlconnect.php');
+
+$ouput = [
+    'success' => false
+];
+
+?>
