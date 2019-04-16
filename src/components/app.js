95b252be5569f80e96725efd0826a6281412e025
WestleyPoon <poonw@uci.edu>
2019-04-16 15:43:55 -0700

    added skeleton for map and nav

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 
+import Nav from './nav';
+import Map from './map';
+
 import '../assets/css/app.scss';
 
 
 const App = () => (
     <div>
         <div className="app center">
+            <Nav/>
             <Switch>
-
+                <Route path='/map' component={Map}/>
             </Switch>
         </div>
     </div>
