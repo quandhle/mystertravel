f52afbaae5dc87d9436e4bd76f51c023a7a6e559
Jennifer Lai <jenniferlai.81@gmail.com>
2019-04-16 21:37:30 -0700

    add map pin endpoint completed with input checks added sql dump that matches dummy data

--- public/api/addmappin.php
+++ public/api/addmappin.php
@@ -2,25 +2,45 @@
 
 require_once('config.php');
 
-//retrieve POST
-//$json_input = file_get_contents("php://input");
-//$input = json_decode($json_input, true);
+$json_input = file_get_contents("php://input");
+$input = json_decode($json_input, true);
 
+$trips_id = intval($input['trips_id']);
+$latitude = floatval($input['latitude']);
+$longitude = floatval($input['longitude']);
+$description = addslashes($input['description']);
 
-$latitude = -117.765991;
-$longitude = 33.728851;
-$trips_id = 1;
+if(empty($trips_id)){
+    throw new Exception('Must provide trips_id (int) with your request');
+}
+if(empty($description)){
+    throw new Exception('Must provide trip description (str) with your request');
+}
+if(empty($latitude) || empty($longitude)){
+    throw new Exception('Must provide location (float) with your request');
+}
 
 $query = "INSERT INTO `pins`
     SET
         `trips_id` = $trips_id,
         `latitude` = $latitude,
         `longitude` = $longitude,
-        `description` = 'LearningFuze Coding Bootcamp',
+        `description` = '$description',
         `added` = NOW()
 ";
 
 $result = mysqli_query($conn, $query);
-print_r($result);
-//return 1
+
+if(!$result){
+    throw new Exception(mysqli_error($conn));
+}
+
+if(mysqli_affected_rows($conn) !== 1){
+    throw new Exception('Map pin was not added');
+}
+
+$output['success'] = true;
+
+print(json_encode($output));
+
 ?>
