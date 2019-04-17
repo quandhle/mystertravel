fe7e6ce6b00a1b8ea3edd230506998600750f0fa
Jennifer Lai <jenniferlai.81@gmail.com>
2019-04-16 18:15:00 -0700

    add map pin endpoint in progress

--- public/api/addmappin.php
+++ public/api/addmappin.php
@@ -2,12 +2,25 @@
 
 require_once('config.php');
 
+//retrieve POST
+//$json_input = file_get_contents("php://input");
+//$input = json_decode($json_input, true);
+
+
+$latitude = -117.765991;
+$longitude = 33.728851;
+$trips_id = 1;
+
 $query = "INSERT INTO `pins`
     SET
-        `trips_id` = 1,
-        `latitude` = 21341,
-        `longitude` = 143243, 
-        `description` = 'LearningFuze Coding Bootcamp'
-        `added` = NOW();
+        `trips_id` = $trips_id,
+        `latitude` = $latitude,
+        `longitude` = $longitude,
+        `description` = 'LearningFuze Coding Bootcamp',
+        `added` = NOW()
 ";
 
+$result = mysqli_query($conn, $query);
+print_r($result);
+//return 1
+?>
