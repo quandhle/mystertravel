c99b81d52e7ed2d6d406611fca3ea4cdc8c7a654
Quan D. Le <46691449+quandhle@users.noreply.github.com>
2019-04-17 10:15:08 -0700

    Update map.js

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,44 +1,5 @@
 import React, {Component} from 'react';
 import './map.scss';
-<<<<<<< HEAD
-import {Map, GoogleApiWrapper} from 'google-maps-react';
-import { isAbsolute } from 'path';
-
-const mapStyles = {
-    map: {
-        position: 'absolute',
-        width: '100%',
-        height: '100%'
-    }
-};
-
-export class MapContainer extends Component {
-    render() {
-        return (
-            <Map
-                google={this.props.google}
-                zoom={13}
-                style={
-                    {
-                        position: 'absolute',
-                        width: '100%',
-                        height: 'calc(100%-56px)'
-                    }
-                }
-                initialCenter={
-                    {lng: -117.765991,
-                    lat: 33.6839473}
-                }
-            />
-        )
-    }
-}
-
-export default GoogleApiWrapper({
-    apiKey: 'AIzaSyAyeel_P_5U9HXS5zEfmEqUvRuegRdqyic'
-})(MapContainer);
-=======
-
 
 class Map extends Component{
     constructor(props){
@@ -96,4 +57,3 @@ function loadScript(url){
 }
 
 export default Map;
->>>>>>> baa020dfb3591ffe9d95f757af30d91672537679
