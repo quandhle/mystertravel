4d9ff35fc0cfd50fec8339bdff980cc39650eb1c
Jennifer Lai <jenniferlai.81@gmail.com>
2019-04-18 12:48:30 -0700

    connected budget page to get budget list, trips_id is hardcoded

--- src/helper/index.js
+++ src/helper/index.js
@@ -1,1 +1,18 @@
 //helper functions goes here
+export function formatMoney(pennies){
+    pennies = parseFloat(pennies);
+    if(isNaN(pennies)){
+        return '-';
+    }
+    const dollars = (pennies/100).toFixed(2);
+    return `$ ${dollars}`;
+}
+
+export function formatEntries(text){
+    let firstChar = text.charAt(0)
+    if(typeof firstChar === 'string'){
+        firstChar = firstChar.toUpperCase();
+        return firstChar + text.slice(1);
+    }
+    return text;
+}
