40a733233b14a9018adc157cfcd1cccb2625bb1f
Kylie Lin <kylieclin@gmail.com>
2019-04-17 21:56:36 -0700

    added basic html for summary added basic css for summary

--- src/components/my_trip/summary/end_trip.js
+++ src/components/my_trip/summary/end_trip.js
@@ -1,11 +1,25 @@
 import React, {Component} from 'react';
+import './summary.scss';
+
+import summaryimg from '../../../assets/images/summary.jpg';
 
 class EndTrip extends Component{
     render(){
         return(
-            <div className="end-trip-page">
-                <div className="end-trip-container">
-                    <div className="end-trip">diary here</div>
+            <div className="endtrip-page">
+                <div className="total-spend">
+                    <p>Total spend in this trip: $1500</p>
+                </div>
+                <div className="last-entry">
+                    <div className="entry-content">
+                        <img src={summaryimg} alt="temp"/>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident omnis aperiam quas ex suscipiteius eveniet molestias reiciendis debitis fuga, magni, dolorum tenetur autem voluptas. Pariaturexplicabo consectetur dignissimos eius.</p>
+                    </div>
+                    <div className="share-btns col-12">
+                        <div className="facebook"></div>
+                        <div className="twitter"></div>
+                        <div className="gmail"></div>
+                    </div>
                 </div>
             </div>
         )
