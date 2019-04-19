5432dc0e13c08f515284dbcecec1c52e8e188475
Kylie Lin <kylieclin@gmail.com>
2019-04-18 19:21:58 -0700

    connect to google autocomplete api

--- src/components/home.js
+++ src/components/home.js
@@ -10,8 +10,10 @@ class Home extends Component {
         super(props)
 
         this.state ={
-            modal: true
+            modal: false
         }
+
+        this.autoComplete;
     }
     goToTrip = () => {
         this.props.history.push(`/mytrip`);
@@ -27,13 +29,24 @@ class Home extends Component {
             })
         }
     }
-    createSearch = ()=>{
-        loadScript(`https://maps.googleapis.com/maps/api/js?key=${"AIzaSyCz5y10D2RANKFguerczz92ZroUQcdLcMI"}&libraries=places`);
-        
-    }
+    componentDidMount(){
 
+    }
+    createSearch = ()=>{
+        loadScript(`https://maps.googleapis.com/maps/api/js?key=${"AIzaSyCz5y10D2RANKFguerczz92ZroUQcdLcMI"}&libraries=places&callback=initAutocomplete`);
+        window.initAutocomplete = this.initAutocomplete;
+    }
+    initAutocomplete = ()=>{
+        const input = document.getElementById("places");
+        this.autoComplete = new window.google.maps.places.Autocomplete(input, {
+            types: ['(regions)']});
+        this.autoComplete.setFields(['address_component']);
+        this.autoComplete.addListener('place_changed', this.searchCountry);
+    }
     searchCountry = (value)=>{
-        console.log(value)
+        var place = this.autoComplete.getPlace();
+        console.log(place['address_components'][0]['long_name'])
+
     }
     render() {
         const {handleSubmit} = this.props
@@ -46,15 +59,14 @@ class Home extends Component {
                     <img src={coconut} alt=""/>
                 </div>
                 <div className="home-page-btn">
-                    <button onClick={this.openModal} className="home-start-btn btn btn-primary">Start / View trip</button>
+                    <button onClick={()=>{this.openModal(); this.createSearch()}} className="home-start-btn btn btn-primary">Start / View trip</button>
                 </div>
 
                 <Modal open={this.state.modal} childrenStyle="home-modal">
                     <div className="homepage-modal-header">Where are you going? </div>
-                    <form onSubmit={handleSubmit(this.searchCountry)}>
+                    <form onSubmit={this.searchCountry}>
                          <Field id="places" name="places" label="Enter Places" component={Input}/>
-                    </form>
-                   
+                    </form> 
                 </Modal>
             </div>
         )
@@ -65,7 +77,8 @@ function loadScript(url){
     const index = window.document.getElementsByTagName("script")[0];
     const script = window.document.createElement("script");
     script.src = url;
-    script.type = "text/javascript";
+    script.async = true;
+    script.defer = true;
     index.parentNode.insertBefore(script, index);
 }
 
