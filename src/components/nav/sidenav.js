3ba68315025c5228ffd0ffd62c2afa09ba3586f3
Kylie <46201510+kylieclin@users.noreply.github.com>
2019-04-21 11:58:07 -0700

    Revert "Add pin"

--- src/components/nav/sidenav.js
+++ src/components/nav/sidenav.js
@@ -1,21 +1,23 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
+
 class SideNav extends Component{
-    render() {
+    render(){
         const {style:{body, background}, toggle} =this.props
 
-        return(
-        <div className="side-nav-box" onClick={toggle} style={background}>
-            <div className="side-nav" style={body}>
-                <div className="side-nav-links">
-                    <Link to="/mytrip">Current Trip</Link>
-                    <Link to="/aboutus">About Us</Link>
-                    <Link to="/">Home</Link>
+
+            return(
+            <div className="side-nav-box" onClick={toggle} style={background}>
+                <div className="side-nav" style={body}>
+                    <ul className="side-nav-links">
+                        <Link to="/mytrip"><li>Current Trip</li></Link>
+                        <Link to="/aboutus"><li>About Us</li></Link>
+                        <Link to="/"><li>Home</li></Link>
+                    </ul>
                 </div>
             </div>
-        </div>
-        )
+            )
     }
 }
 
