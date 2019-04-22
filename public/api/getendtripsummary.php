10b60be5d0e2d33f18e8f1b7fc5fff48707d1a57
WestleyPoon <poonw@uci.edu>
2019-04-22 13:08:38 -0700

    hooked up summary page to its own endpoint

--- public/api/getendtripsummary.php
+++ public/api/getendtripsummary.php
@@ -4,7 +4,7 @@ require_once('config.php');
 
 $trips_id = $_GET['trips_id'];
 
-$end_trip_query = "SELECT t.`trips_name`, t.`region`,
+$summary_query = "SELECT t.`trips_name`, t.`region`,
     SUM(b.`price`) AS 'total_budget',
         (SELECT `entry`
         FROM `notes`
@@ -18,24 +18,24 @@ $end_trip_query = "SELECT t.`trips_name`, t.`region`,
     WHERE t.`id` = ?
 ";
 
-$end_trip_statement = mysqli_prepare($conn, $end_trip_query);
-mysqli_stmt_bind_param($end_trip_statement, 'd', $trips_id);
-mysqli_stmt_execute($end_trip_statement);
+$summary_statement = mysqli_prepare($conn, $summary_query);
+mysqli_stmt_bind_param($summary_statement, 'd', $trips_id);
+mysqli_stmt_execute($summary_statement);
 
-$end_trip_result = mysqli_stmt_get_result($end_trip_statement);
+$summary_result = mysqli_stmt_get_result($summary_statement);
 
-if (!$end_trip_result) {
+if (!$summary_result) {
     throw new Exception(mysqli_error($conn));
 }
 
-if (mysqli_num_rows($end_trip_result) === 0) {
+if (mysqli_num_rows($summary_result) === 0) {
     $output['success'] = true;
     $output['tasks'] = [];
     print(json_encode($output));
     exit();
 }
 
-$row = mysqli_fetch_assoc($update_result);
+$row = mysqli_fetch_assoc($summary_result);
 
 $output['success'] = true;
 $output['data'] = [
