96aaba9bd5e1a663c3832f7fdaf5d69f7a3e6e40
WestleyPoon <poonw@uci.edu>
2019-04-23 15:58:13 -0700

    added facebook and twitter button functionality

--- src/components/my_trip/summary/summary.js
+++ src/components/my_trip/summary/summary.js
@@ -3,8 +3,9 @@ import axios from 'axios';
 import {connect} from 'react-redux';
 
 import './summary.scss';
-import {formatMoney} from './../../../helper/index';
+import {formatMoney} from './../../../helper';
 import {clearTripId} from "../../../actions";
+import {loadScript} from "./../../../helper";
 
 import summaryimg from '../../../assets/images/summary.jpg';
 import thinkingEmoji from '../../../assets/images/thinking-emoji.png';
@@ -28,6 +29,18 @@ class EndTrip extends Component{
 
     componentDidMount() {
         this.getSummaryData();
+        
+        loadScript('https://connect.facebook.net/en_US/sdk.js');
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId            : '2052844328350812',
+                autoLogAppEvents : true,
+                xfbml            : true,
+                version          : 'v3.2'
+            });
+        };
+
+        loadScript('https://platform.twitter.com/widgets.js');
     }
 
     async getSummaryData() {
@@ -59,20 +72,21 @@ class EndTrip extends Component{
         }
     }
 
-    fbButton = () => {
-        const trips_id = this.paramTripsId ? this.paramTripsId : this.props.trips_id.trips_id;
-        console.log(`${window.location.origin}/trip/${trips_id}`);
+    fbButton = (url) => {
+        FB.ui({
+            method: 'share',
+            display: 'popup',
+            href: url,
+        }, function(response){});
     }
 
-    twitterButton = () => {
-
+    twitterButton = (url) => {
+        return encodeURIComponent(`I went on a trip recently! Check it out on MysterTravel!\n${url}`);
     }
 
-    mailButton() {
-        const trips_id = this.paramTripsId ? this.paramTripsId : this.props.trips_id.trips_id;
-
+    mailButton(url) {
         return `mailto: ?subject=${'I went on a trip!'}&body=${
-            `I went on a trip recently! Check it out on MysterTravel!%0D%0A${window.location.origin}/trip/${trips_id}`
+            encodeURIComponent(`I went on a trip recently! Check it out on MysterTravel!\n${url}`)
         }`;
     }
 
@@ -80,6 +94,7 @@ class EndTrip extends Component{
         const {tripName, totalSpent, lastNote} = this.state;
         const trips_id = this.paramTripsId ? this.paramTripsId : this.props.trips_id.trips_id;
         const paramTripsId = this.paramTripsId;
+        const summaryURL = `http://www.mystertravel.com/trip/${trips_id}`;
 
         if (trips_id > 0 && tripName) {
             return(
@@ -96,12 +111,18 @@ class EndTrip extends Component{
                             <p>{lastNote}</p>
                         </div>
                         <div className="share-btns col-12">
-                            <div className="facebook">
-                                <a onClick={this.fbButton}><i className="fab fa-facebook-square"></i></a>
+                            <div className="facebook" id='facebook-div'>
+                                <a onClick={() => {this.fbButton(summaryURL)}}><i className="fab fa-facebook-square"></i></a>
                             </div>
-                            <div className="twitter"><i className="fab fa-twitter-square"></i></div>
+                            <div className="twitter">
+                                <a href={`https://twitter.com/intent/tweet?text=${this.twitterButton(summaryURL)}`}>
+                                    <i className="fab fa-twitter-square"></i>
+                                </a>
+                            </div>
                             <div className="gmail">
-                                <a href={this.mailButton()}><i className="fas fa-envelope-square"></i></a>
+                                <a href={this.mailButton(summaryURL)}>
+                                    <i className="fas fa-envelope-square"></i>
+                                </a>
                             </div>
                         </div>
                     </div>
