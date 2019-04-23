1330d5b9ece38aa03f415ba0cf6fe2dc364fd9af
quandl <quandhle@gmail.com>
2019-04-22 20:21:56 -0700

    infowindow content now unique to pin

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -23,7 +23,6 @@ class Map extends Component {
     }
 
     componentDidMount() {
-        // if it isn't already loaded, load Google Maps API script, and then initialize the map
         if (!(window.google && window.google.maps)) {
             this.loadMapScript();
         } else {
@@ -32,26 +31,21 @@ class Map extends Component {
     }
 
     loadMapScript() {
-        // loads the Google Maps API script
-        // a callback function is given to the API, which is run when the script is ready to load the map
         loadScript(`https://maps.googleapis.com/maps/api/js?key=${keys.googleMaps}&libraries=places&callback=initMap`);
         window.initMap = this.initMap;
     }
 
     initMap = () => {
-        // the input element you want autocomplete on
         const searchBarInput = document.getElementById("places");
         this.autoComplete = new window.google.maps.places.Autocomplete(searchBarInput, {
             types: ['(regions)']
         });
         this.autoComplete.setFields(['address_component', 'geometry', 'name']);
 
-        // if autocomplete input changes, run the event handler
         this.autoComplete.addListener('place_changed', this.searchCountry);
 
-        // initialize the map
         const map = new window.google.maps.Map(document.getElementById('map'), {
-            mapTypeControl: false,      // hide some of the control buttons
+            mapTypeControl: false,
             streetViewControl: false,
             fullscreenControl: false,
             center: {lat: this.state.lat, lng: this.state.lng},
@@ -59,7 +53,6 @@ class Map extends Component {
             minZoom: 2,
         });
 
-        // save the map in state and get/show pins
         this.setState({map: map});
         this.showPins();
     }
@@ -68,30 +61,24 @@ class Map extends Component {
 
         let zipCodeOffset = 0;
         if (address_components[address_components.length - 1].types[0] === 'postal_code') {
-            // if the last slot is a zip code, we'll have to offset the cases below by 1 to skip over the zip code
             zipCodeOffset++;
         }
 
         if (address_components[address_components.length - 1 - zipCodeOffset].short_name === 'US') {
-            // if the location is in the US:
             switch(address_components.length) {
 
                 case 3 + zipCodeOffset:
                 case 2 + zipCodeOffset:
-                    // if only state and country are present, return 'state', US
                     return `${address_components[address_components.length - 2 - zipCodeOffset].long_name}, United States`;
 
                 case 1 + zipCodeOffset:
-                    // if the search was just 'US'
                     return 'United States';
 
                 default:
-                    // otherwise, return 'city, state'
                     return `${address_components[0].short_name}, ${address_components[address_components.length - 2 - zipCodeOffset].short_name}`;
             }
 
         } else {
-            // for non-US countries, append all available parts
             return address_components.map((item) => {
                 return item.long_name;
             }).join(', ');
@@ -99,22 +86,17 @@ class Map extends Component {
     }
 
     searchCountry = () => {
-        // grab the input from the searchbar (either selected autocomplete entry or user key input)
         const place = this.autoComplete.getPlace();
 
         let address, location;
 
         if (place.address_components) {
-            // autocompleted Places will have a address_components and a geometry.location (coords)
             address = this.parseAddressComponents(place.address_components);
             location = place.geometry.location;
 
-            // center the map on the result
             this.state.map.setCenter(location);
             this.state.map.setZoom(12);
         } else {
-            // user key input is put into Place.name
-            // use findPlaceFromQuery to find a suitable match
             const service = new google.maps.places.PlacesService(this.state.map);
             service.findPlaceFromQuery({
                     query: place.name,
@@ -122,7 +104,6 @@ class Map extends Component {
 
             }, (results, status) => {
                 if (status === google.maps.places.PlacesServiceStatus.OK) {
-                    // center the map on the result
                     this.state.map.setCenter(results[0].geometry.location);
                     this.state.map.setZoom(12);
                 }
@@ -131,7 +112,6 @@ class Map extends Component {
             address = place.name;
         }
 
-        // change the input field to have the new address result
         this.props.dispatch(change("search-bar-form", `places`, address));
 
         this.setState({
@@ -142,17 +122,15 @@ class Map extends Component {
     }
 
     async showPins() {
-        // hard coded trips id, grab this from redux later
         const trips_id = 1;
 
-        // get the pin data from the server
         const resp = await axios.get(`/api/getmappin.php?trips_id=${trips_id}`);
         let pinData = null;
-        if(resp.data.success) {
+         
+        if (resp.data.success) {
             pinData = resp.data.data;
         }
 
-        // if there was data, turn the pin data into Marker objects
         if (pinData) {
             const pins = pinData.map((item) => {
                 const pin = new window.google.maps.Marker({
@@ -163,20 +141,20 @@ class Map extends Component {
                     map: this.state.map
                 });
 
-                // display the markers
-                pin.setMap(this.state.map);
+                const content = ('<h4 id="infoWindow">' + item.name + '</h4><p>' + item.description + '</p>');
 
                 const infowindow = new google.maps.InfoWindow({
-                    content: this.state.name
+                    content: content
                 })
 
                 pin.addListener('click', function() {
                     infowindow.open(map, pin);
                 })
+
+                pin.setMap(this.state.map);
                 
                 return pin;
             });
-            // save all the Marker objects in state
             this.setState({
                 pins: pins,
             });
@@ -190,7 +168,6 @@ class Map extends Component {
             this.setState(coordObject);
             this.state.map.setCenter(coordObject);
 
-            // get address
             const geocoder = new google.maps.Geocoder;
             geocoder.geocode({'location': coordObject}, (results, status) => {
                 if (status === 'OK') {
@@ -220,7 +197,7 @@ class Map extends Component {
             description: 'This is Irvine.',
             name: name
         }).then((resp) => {
-            const pin = new window.google.maps.Marker({
+            const marker = new window.google.maps.Marker({
                 position: {
                     lat: lat,
                     lng: lng,
@@ -228,13 +205,9 @@ class Map extends Component {
                 title: resp.data.name
             });
 
-            console.log('resp is: ', resp.data);
-
             this.setState({
-                pins: [...this.state.pins, pin]
+                pins: [...this.state.pins, marker]
             })
-
-            console.log('state is: ', this.state);
         })
 
         this.showPins();
@@ -248,7 +221,7 @@ class Map extends Component {
                 </div>
                 <div id="map" className='map'/>
                     <button className='btn map-btn btn-lg' onClick={this.addPin}>
-                        <i className="fas fa-map-marker-alt"/> Add Pin
+                        Add Pin <i className="fas fa-map-marker-alt"/>
                     </button>
                 <button onClick={this.getCurrentLocation} className='btn geo-btn btn-lg'>
                     <i className="fas fa-location-arrow"/>
@@ -261,6 +234,7 @@ class Map extends Component {
 function loadScript(url){
     const index = window.document.getElementsByTagName("script")[0];
     const script = window.document.createElement("script");
+
     script.src = url;
     script.async = true;
     script.defer = true;
