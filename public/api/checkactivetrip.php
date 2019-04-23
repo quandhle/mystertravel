73fc77d987fa5096b41b37ed4d5486121731817f
Jennifer Lai <jenniferlai.81@gmail.com>
2019-04-22 19:05:39 -0700

    output more user data for guest, add trips_id in session for login users

--- public/api/checkactivetrip.php
+++ public/api/checkactivetrip.php
@@ -2,16 +2,6 @@
 
 require_once('config.php');
 
-// if (!empty($_SESSION['user_data'])) {
-//     $token = $_SESSION['user_data']['token'];
-// } else {
-//     $token = $_GET['token'];
-// }
-
-// if(empty($users_id)){
-//     throw new Exception('Please provide users_id (int) with your request');
-// }
-
 $query = "SELECT `id`, `end`
     FROM `trips`
     WHERE `users_id` = ?
@@ -36,17 +26,10 @@ if(mysqli_num_rows($result) === 0){
 $row = mysqli_fetch_assoc($result);
 
 if (empty($row['end'])) {
-    //$output['success'] = true;
     $output['trips_id'] = $row['id'];
-    // print(json_encode($output));
-    // exit();
+    $_SESSION['user_data']['trips_id'] = $row['id'];
 } else {
     $output['trips_id'] = null;
 }
-
-//trip has an end date, trip is inactive
-//$output['success'] = true;
-
-//print(json_encode($output));
 
 ?>
