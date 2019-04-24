a9b7f639eb6c944290dbe5cff49fbca955d38f81
Kylie Lin <kylieclin@gmail.com>
2019-04-24 12:52:40 -0700

    add sign up pageform and style

---
+++ src/components/account/sign_up/sign_up.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import {connect} from 'react-redux';
+import {signIn, passTripId} from '../../../actions';
+import SignUpForm from './signup_form';
+import './sign_up.scss';
+
+class SignIn extends Component{
+    constructor(props){
+        super(props);
+
+        this.handleSignUp = this.handleSignUp.bind(this);
+    }
+    async handleSignUp (values){
+        const {email, password} = values
+        const resp = await axios.post('', {
+            email,
+            password
+        })
+
+
+    }
+    render(){
+        return(
+            <div className="sign-up-page">
+                <SignUpForm signUp={this.handleSignUp}/>
+            </div>
+        )
+    }
+}
+
+export default connect(null, {signIn, passTripId})(SignIn);
