99010ce0e954ba4b27964213c26a9bf5c8f6f3d6
WestleyPoon <poonw@uci.edu>
2019-04-25 16:32:17 -0700

    summary page timeline reads from db

--- src/components/my_trip/summary/timeline.js
+++ src/components/my_trip/summary/timeline.js
@@ -5,12 +5,25 @@ import TimelineInner from './timeline_inner';
 
 export default class Timeline extends Component {
     render() {
+        const {pinData, notesData} = this.props;
+        let timelineItems;
+
+        if (pinData && notesData) {
+            timelineItems = [...pinData, ...notesData];
+            timelineItems.sort(function(a,b) {
+                return new Date(a.date) - new Date(b.date);
+            });
+        }
+
+        const firstItem = (timelineItems && timelineItems.length) ? timelineItems.shift() : null;
+        const lastItem = (timelineItems && timelineItems.length) ? timelineItems.pop() : null;
+
         return (
             <div className="timeline">
                 <p className="timeline-title">Trip Timeline</p>
-                <TimelineItem/>
-                <TimelineInner/>
-                <TimelineItem/>
+                <TimelineItem item={firstItem}/>
+                <TimelineInner items={timelineItems}/>
+                <TimelineItem item={lastItem}/>
             </div>
         );
     }
--- src/components/my_trip/summary/timeline_item.js
+++ src/components/my_trip/summary/timeline_item.js
@@ -1,23 +1,47 @@
 import React, {Component} from 'react';
 
+import {formatDate} from "../../../helper";
+
 export default class extends Component {
     state = {
+        isNote: false,
+        date: null,
 
+        note_id: null,
+        image: null,
+        entry: null,
+
+        pin_id: null,
+        name: null,
+        description: null,
+        lat: null,
+        lng: null
     };
 
-    render() {
-        const {onClick} = this.props;
+    componentDidUpdate(prevProps) {
+        const {item} = this.props;
+        if (item !== prevProps.item) {
+            const isNote = !!item.note_id;
+            this.setState({isNote, ...item});
+        }
+    }
 
-        return (
-            <div className="timeline-item">
-                <div className="timeline-item-content">
-                    <p className="timeline-item-name">Spectrum</p>
-                    <p className="timeline-item-date">99/99/99</p>
-                    <p className="timeline-item-text">Wow there was a lot of stuff. Wow there was a lot of stuff. Wow
-                        there was a lot of stuff. Wow there was a lot of stuff. Wow there was a lot of stuff. Wow there
-                        was a lot of stuff. </p>
+    render() {
+        const {item} = this.props;
+        if (item && item.hasOwnProperty('date')) {
+            const isNote = !!item.note_id;
+
+            return (
+                <div className="timeline-item">
+                    <div className="timeline-item-content">
+                        <p className="timeline-item-name">{item.name ? item.name : null}</p>
+                        <p className="timeline-item-date">{formatDate(item.date)}</p>
+                        <p className="timeline-item-text">{isNote ? item.entry : item.description}</p>
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        } else {
+            return null;
+        }
     }
 }
