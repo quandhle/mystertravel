640349644be4e73884730336a1e45f96359b0bbf
Kylie Lin <kylieclin@gmail.com>
2019-04-29 16:58:19 -0700

    remove map add pin button add map cursor

--- src/components/general/input.js
+++ src/components/general/input.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './general.scss';
 
 export default props =>{
-    const {id, name, classes = "", type = "text", label, col="col-12", input, meta:{error, touched}, autoFocus = false} = props
+    const {id, name, classes = "", type = "text", label, col="col-12", input, meta:{error, touched}, autoFocus = false, min="0"} = props
 
     return (
         <div className={`input-box ${classes} ${col}`}>
-            <input type={type} id={id} {...input} name={name} placeholder={label} autoComplete="off" autoFocus={autoFocus}/>
+            <input type={type} id={id} {...input} name={name} placeholder={label} autoComplete="off" autoFocus={autoFocus} min="0"/>
             <p className="error-input">{touched && error}</p>  
         </div>
     )
--- src/components/my_trip/budget/budget_form.js
+++ src/components/my_trip/budget/budget_form.js
@@ -6,9 +6,9 @@ const BudgetForm = props => {
     const { budget, handleSubmit, style} = props;
         return(
             <form onSubmit={handleSubmit(budget)} className="budget-input-form" style={style}>
-                    <Field id="description" name="description" label="Description" component={Input} classes="budget-input"/>
-                    <Field id="price" name="price" label="Amount" component={Input} classes="budget-input"/>
-                    <Field id="category" name="category" label="Category" component={Input} classes="budget-input"/>
+                <Field id="description" name="description" label="Description" component={Input} classes="budget-input"/>
+                <Field id="price" name="price" label="$ Amount" component={Input} classes="budget-input" type="number" min="0"/>
+                <Field id="category" name="category" label="Category" component={Input} classes="budget-input"/>
                 <button className="btn add-budget">Add <i className="fas fa-check"></i></button>
             </form>
         )
