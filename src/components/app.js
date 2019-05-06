067bb14bd2133ef867229ec183e0c24a47a27577
WestleyPoon <poonw@uci.edu>
2019-05-06 16:58:02 -0700

    auth hoc implemented on routes

--- src/components/app.js
+++ src/components/app.js
@@ -9,6 +9,7 @@ import AboutUs from './about_us';
 import NotFound from './404';
 import Account from './account';
 import Summary from './my_trip/summary';
+import auth from '../hoc/auth';
 
 import '../assets/css/app.scss';
 
@@ -18,8 +19,8 @@ const App = () => (
             <Nav/>
             <Switch>
                 <Route exact path='/' component={Home}/>
-                <Route path='/map' component={Map}/>
-                <Route path='/mytrip' component={MyTripRoutes}/>
+                <Route path='/map' component={auth(Map)}/>
+                <Route path='/mytrip' component={auth(MyTripRoutes)}/>
                 <Route path='/aboutus' component={AboutUs}/>
                 <Route path='/account' component={Account}/>
                 <Route path='/trip/:date/:tripname/:trips_id' component={Summary}/>
