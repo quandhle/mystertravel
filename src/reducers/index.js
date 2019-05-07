2d7f6b3762d822b9fd01ee3351e52cc1ce527faf
WestleyPoon <poonw@uci.edu>
2019-05-07 10:58:56 -0700

    removed extaneous files removed unused images removed package.jsons and renamed affected components to index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,6 @@
 import {combineReducers} from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import userReducer from './user_reducer';
-import tripReducer from './trip_reducer';
-import navReducer from './nav_reducer';
-import UpdateReducer from './update_reducer';
 
 const rootReducer = combineReducers({
     form: formReducer,
