0a71b82969cab2f9087a85d3fc675ddd6e85c01c
WestleyPoon <poonw@uci.edu>
2019-05-08 13:19:14 -0700

    added date and money sort buttons to budget page

--- src/components/my_trip/budget/index.js
+++ src/components/my_trip/budget/index.js
@@ -17,13 +17,24 @@ class Budget extends Component{
                 height: 0
             },
             budget: [],
-            spinner: false
+            spinner: false,
+            sortMode: 'date',
+            sortByDateIcon: ['down', 'up'],
+            dateButton: {
+                'backgroundColor': '#fa8d62'
+            },
+            sortByMoneyIcon: ['down', 'up'],
+            moneyButton: {
+                'backgroundColor': '#2b616d'
+            }
         };
 
         this.toggleInput = this.toggleInput.bind(this);
         this.handleInput = this.handleInput.bind(this);
         this.deleteBudgetItem = this.deleteBudgetItem.bind(this);
         this.getBudgetList = this.getBudgetList.bind(this);
+        this.sortBudgetByDate = this.sortBudgetByDate.bind(this);
+        this.sortBudgetByMoney = this.sortBudgetByMoney.bind(this);
     }
 
     async handleInput(value) {
@@ -100,12 +111,59 @@ class Budget extends Component{
         }
     }
 
+    sortBudgetByDate() {
+        const {sortMode, sortByDateIcon} = this.state;
+
+        switch (sortMode) {
+            case 'date':
+                this.setState({
+                    sortByDateIcon: this.state.sortByDateIcon.reverse(),
+                    budget: this.state.budget.reverse()
+                });
+                break;
+            case 'money':
+                this.setState({
+                    sortMode: 'date',
+                    dateButton: {'backgroundColor': '#fa8d62'},
+                    moneyButton: {'backgroundColor': '#2b616d'},
+                    budget: sortByDateIcon[0] === 'down' ?
+                        this.state.budget.sort((a, b) => {return a.added - b.added}) :
+                        this.state.budget.sort((a, b) => {return b.added - a.added})
+
+                });
+                break;
+        }
+    }
+
+    sortBudgetByMoney() {
+        const {sortMode, sortByMoneyIcon} = this.state;
+
+        switch (sortMode) {
+            case 'date':
+                this.setState({
+                    sortMode: 'money',
+                    dateButton: {'backgroundColor': '#2b616d'},
+                    moneyButton: {'backgroundColor': '#fa8d62'},
+                    budget: sortByMoneyIcon[0] === 'down' ?
+                        this.state.budget.sort((a, b) => {return b.price - a.price}) :
+                        this.state.budget.sort((a, b) => {return a.price - b.price})
+                });
+                break;
+            case 'money':
+                this.setState({
+                    sortByMoneyIcon: this.state.sortByMoneyIcon.reverse(),
+                    budget: this.state.budget.reverse()
+                });
+                break;
+        }
+    }
+
     componentDidMount() {
         this.getBudgetList();
     }
 
     render() {
-        const {budget, showInput, spinner} = this.state;
+        const {budget, showInput, spinner, sortByMoneyIcon, sortByDateIcon} = this.state;
         let budgetList = null;
 
         if(budget.length > 0) {
@@ -124,6 +182,16 @@ class Budget extends Component{
                     Add Budget Item <i className="fas fa-angle-double-down"></i>
                     </div>
                     <BudgetForm budget={this.handleInput} style={showInput}/>
+                    <div className="sort-budget">
+                        <button className="sort-btn btn" style={this.state.dateButton} onClick={this.sortBudgetByDate}>
+                            <i className={'fas fa-clock'}></i>
+                            <i className={`fas fa-sort-${sortByDateIcon[0]}`}></i>
+                        </button>
+                        <button className="sort-btn btn" style={this.state.moneyButton} onClick={this.sortBudgetByMoney}>
+                            <i className={'fas fa-dollar-sign'}></i>
+                            <i className={`fas fa-sort-${sortByMoneyIcon[0]}`}></i>
+                        </button>
+                    </div>
                     <div className="budget-box">
                         {budgetList}
                     </div>
